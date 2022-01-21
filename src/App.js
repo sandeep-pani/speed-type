a0b440e10b3aca2fe1a6fc1a5ed9a85eda762752
sandeep-pani <80412459+sandeep-pani@users.noreply.github.com>
2022-01-22 00:39:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import Content from "./components/content.js";
 import SpeedType from "./components/speedType.js";
 
 function App() {
-  console.log("url:" + process.env.PUBLIC_URL);
   return (
     <div>
-      <h1>hello</h1>
       <Router>
         <NavBar />
-        a:{`/${process.env.PUBLIC_URL}`}
-        <Routes basename={process.env.PUBLIC_URL}>
+        <Routes basename={process.env["PUBLIC_URL"]}>
           <Route path="/" element={<Content />} />
           <Route path="/begin-main" element={<SpeedType />} />
         </Routes>
